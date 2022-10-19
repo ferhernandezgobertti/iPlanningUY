378f4deabc1b682ea8152024ce5612639a7aa4b3
ferhernandezgobertti <38531693+ferhernandezgobertti@users.noreply.github.com>
2022-10-19 15:05:37 -0300

    Add files via upload
    DataBase files added

---
+++ Application_Code/DataBase/Context.cs
@@ -0,0 +1,28 @@
+﻿using Domain;
+using System.Data.Entity;
+
+namespace DataBase
+{
+    public class Context : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Designer> Designers { get; set; }
+        public DbSet<Architect> Architects { get; set; }
+        public DbSet<Chart> Charts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            ModelCharts(modelBuilder);
+
+        }
+        private void ModelCharts(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Chart>()
+           .HasMany<IDrawable>(c => c.Elements);
+
+            modelBuilder.Entity<Chart>()
+           .HasMany<Signature>(c => c.Signatures);
+        }
+    }
+}
